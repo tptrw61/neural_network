2fa0a51ed8261f173792a5f3ad21ef1add0a62b7
tptrw61 <tptrw.61@gmail.com>
2020-01-22 10:51:15 -0700

    updated nn.h

--- nn.h
+++ nn.h
@@ -1,7 +1,20 @@
 #pragma once
 #include "linalg.h"
+#include <vector>
 
 class NeuralNetwork {
-public:
 private:
+	std::vector<Matrix> weights;
+	std::vector<Matrix> dw;
+	std::vector<Vector> biases;
+	std::vector<Vector> db;
+	int layers;
+	std::vector<int> layerSize;
+public:
+	NeuralNetwork();
+	~NeuralNetwork();
+
+	//setup
+	//testing
+	//serialization
 };
